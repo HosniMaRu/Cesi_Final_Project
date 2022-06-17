7b264d1b5b3c531fba9ff4bcd3f6138e3c9b06f0
Hosni Marco <hosni.marco@gmail.com>
2022-06-17 13:54:24 +0200

    Html forced dashboard

--- dashboard/todoList.php
+++ dashboard/todoList.php
@@ -1,3 +1,13 @@
 <?php
 date_default_timezone_set('Europe/Madrid');
 require_once $_SERVER["DOCUMENT_ROOT"] . "config/admin.php";
+
+switch ($_POST['']) {
+    case 'value':
+        # code...
+        break;
+
+    default:
+        # code...
+        break;
+}
