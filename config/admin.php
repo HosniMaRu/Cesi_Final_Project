12d72197813498bfb3b0bfc4bdc81839136d5426
Hosni Marco <hosni.marco@gmail.com>
2022-06-28 08:57:26 +0200

    vendor

--- config/admin.php
+++ config/admin.php
@@ -1,6 +1,6 @@
 <?php
 define("RECAPTCHA_V3_SECRET_KEY", '6LemHlMgAAAAAL9dq7CKAZhtH-VGp_-460Em0rQU');
-define("MYSQL_SERVER", "localhost");
-define("MYSQL_DDBB", "root");
-define("MYSQL_PASSWORD", "");
-define("MYSQL_TABLE", "pbd");
+define("MYSQL_SERVER", "sql11.freemysqlhosting.net");
+define("MYSQL_DDBB", "sql11502507");
+define("MYSQL_PASSWORD", "tgBspvzaDK");
+define("MYSQL_TABLE", "sql11502507");
